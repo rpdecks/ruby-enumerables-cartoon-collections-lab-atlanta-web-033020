fc65200c24eecaf0bb0195331564603b627d44d0
Robert Phillips <rpdecks@gmail.com>
2020-03-20 16:22:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,7 +20,7 @@ end
 def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.include? do |i| 
+  array.include?(cheese_types) do |i| 
     
   end
 end
