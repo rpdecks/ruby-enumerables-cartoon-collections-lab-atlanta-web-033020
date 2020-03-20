37238081403d25b4c1c1b8a56230b8238d8f68b6
Robert Phillips <rpdecks@gmail.com>
2020-03-20 16:15:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,8 +11,10 @@ def summon_captain_planet(array)
   end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any? |i| do
+    i.
+  end
 end
 
 def find_the_cheese# code an argument here
